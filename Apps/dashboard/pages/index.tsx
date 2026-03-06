a97115edd2ec75ab9c57956d49e20173baefa617
bukki-agent <bukki.ai.agent@gmail.com>
2026-03-06 07:21:57 +0100

    Create index.tsx

---
+++ Apps/dashboard/pages/index.tsx
@@ -0,0 +1,6 @@
+import React, { useState } from 'react';
+import BrainPanel from '../components/BrainPanel';
+import AgentMatrix from '../components/AgentMatrix';
+import KPIDeck from '../components/KPIDeck';
+import Header from '../components/Header';
+import Sidebar from '../components/Sidebar';
